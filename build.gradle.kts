e3d0c206230f428d8876345244801585947dcf7a
GitHub Action <action@github.com>
2024-06-23 08:04:56 +0000

    Bump version to 2.9.48 (209048)

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.9.47"
-            versionCode = 209047
+            versionName = "2.9.48"
+            versionCode = 209048
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
