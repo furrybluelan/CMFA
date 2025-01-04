f5bef194944cc53c239ed30dc5c9e5221def0fe3
embylee <embylee@gmail.om>
2025-01-04 18:43:44 +0800

    q

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ subprojects {
             minSdk = 21
             targetSdk = 31
 
-            versionName = "2.9.75"
-            versionCode = 209075
+            versionName = "2.11.5"
+            versionCode = 211005
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
